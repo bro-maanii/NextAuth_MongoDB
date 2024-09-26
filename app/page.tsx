421fb2693c7dce1f41abede238f0762949d274a2
bro-maanii <emanaliabbasi31@gmail.com>
2024-09-26 14:30:00 +0500

    Home Page Change Text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="p-16">
-      <h1 className="text-center text-4xl">NEXTtttt AUTH AND MONGODB</h1>
+      <h1 className="text-center text-4xl">NEXT AUTH AND MONGODB</h1>
     </main>
   );
 }
